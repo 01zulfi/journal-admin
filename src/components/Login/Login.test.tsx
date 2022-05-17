b33ffcc18e4b6e3df5193b5779ee38fd5bf8c503
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-17 19:37:57 +0500

    Test for login success/failure

--- src/components/Login/Login.test.tsx
+++ src/components/Login/Login.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import Login from './index';
 
 describe('tests login component', () => {
   it('shows correct username on input field after typing', () => {
-    render(<Login />);
+    render(<Login onLogin={() => {}} />);
     const usernameField = screen.getByLabelText('Username:');
 
     userEvent.type(usernameField, 'cool-username');
@@ -15,11 +15,47 @@ describe('tests login component', () => {
   });
 
   it('shows correct password on input field after typing', () => {
-    render(<Login />);
+    render(<Login onLogin={() => {}} />);
     const passwordField = screen.getByLabelText('Password:');
 
     userEvent.type(passwordField, 'secret-password');
 
     expect(passwordField).toHaveValue('secret-password');
+  });
+
+  /* eslint-disable */
+  it('calls onLogin prop when login is successful', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ token: 'secret' }),
+        status: 200,
+      }),
+    ) as jest.Mock;
+    const mockOnLogin = jest.fn();
+
+    render(<Login onLogin={mockOnLogin} />);
+    const loginButton = screen.getByRole('button', { name: 'Login' });
+
+    userEvent.click(loginButton);
+
+    await waitFor(() => expect(mockOnLogin).toBeCalledTimes(1));
+  });
+
+  it('renders error message when login is unsuccessful', async () => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({ message: 'Incorrect username or password' }),
+        status: 401,
+      }),
+    ) as jest.Mock;
+
+    render(<Login onLogin={() => {}} />);
+    const loginButton = screen.getByRole('button', { name: 'Login' });
+
+    userEvent.click(loginButton);
+
+    const errorText = await screen.findByText('Incorrect username or password');
+    expect(errorText).toBeInTheDocument();
   });
 });
