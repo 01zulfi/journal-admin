53f2aed27db7f21c166a7faff966d8a955a7adeb
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-18 20:04:25 +0500

    Add journal interface

---
+++ src/interfaces/journal.ts
@@ -0,0 +1,12 @@
+interface JournalInterface {
+  title: String;
+  content: String;
+  date: String;
+  publish: Boolean;
+  author: String;
+  createdAt: Date;
+  updatedAt: Date;
+  _id: String;
+}
+
+export default JournalInterface;
