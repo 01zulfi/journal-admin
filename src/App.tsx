33675badeb8373346a34556d9a2ab3aea3c63ad8
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-19 19:34:22 +0500

    Update App component * Put together all of the components * Manage logged in state * Handle modals for forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,57 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import Login from './components/Login';
+import Journals from './components/Journals';
+import Modal from './components/Modal';
+import NewJournalForm from './components/NewJournalForm';
+import EditJournalForm from './components/EditJournalForm';
 
 const App: FC = function App() {
-  return <h1>Journal Admin</h1>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showNewJournalForm, setShowNewJournalForm] = useState(false);
+  const [showEditJournalForm, setShowEditJournalForm] = useState(false);
+  const [editJournalId, setEditJournalId] = useState('');
+
+  if (!isLoggedIn) {
+    return (
+      <section>
+        <Login onLogin={() => setIsLoggedIn(true)} />
+      </section>
+    );
+  }
+
+  if (showNewJournalForm) {
+    return (
+      <section>
+        <Modal closeModal={() => setShowNewJournalForm(false)}>
+          <NewJournalForm />
+        </Modal>
+      </section>
+    );
+  }
+
+  if (showEditJournalForm) {
+    return (
+      <section>
+        <Modal closeModal={() => setShowEditJournalForm(false)}>
+          <EditJournalForm journalId={editJournalId} />
+        </Modal>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <button type="button" onClick={() => setShowNewJournalForm(true)}>
+        New
+      </button>
+      <Journals
+        onEditButtonClick={(journalId: string) => {
+          setEditJournalId(journalId);
+          setShowEditJournalForm(true);
+        }}
+      />
+    </section>
+  );
 };
 
 export default App;
